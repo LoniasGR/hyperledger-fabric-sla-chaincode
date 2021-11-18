3c70233983c4f62ad825a1b8e9c638786480b592
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-18 22:36:45 +0200

    Create sample producer and consumer

---
+++ testers/consumer/sample-consumer.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type SLA struct {
+	Customer string `json:"Customer"`
+	ID       string `json:"String"`
+	Metric   string `json:"Metric"`
+	Provider string `json:"Provider"`
+	Status   int    `json:"Status"`
+	Value    int    `json:"Value"`
+}
+
+func main() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{"localhost:9092"},
+		Topic:     "SLA",
+		Partition: 0,
+		MinBytes:  10e3, // 10KB
+		MaxBytes:  10e6, // 10MB
+	})
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		cleanup(r)
+		os.Exit(0)
+	}()
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+
+		var s SLA
+		err = json.Unmarshal(m.Value, &s)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("message at offset %d: %s", m.Offset, string(m.Key))
+		fmt.Println(s)
+
+	}
+	time.Sleep(time.Second)
+
+}
+
+func cleanup(r *kafka.Reader) {
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
--- testers/producer/sample-producer.go
+++ testers/producer/sample-producer.go
@@ -26,12 +26,12 @@ func main() {
 	}
 
 	assets := []SLA{
-		{ID: "asset1", Customer: "blue", Metric: "Downtime", Provider: "Tomoko", Value: 300, Status: 1},
-		{ID: "asset2", Customer: "red", Metric: "Downtime", Provider: "Brad", Value: 400, Status: 1},
-		{ID: "asset3", Customer: "green", Metric: "Downtime", Provider: "Jin Soo", Value: 500, Status: 1},
-		{ID: "asset4", Customer: "yellow", Metric: "Downtime", Provider: "Max", Value: 600, Status: 1},
-		{ID: "asset5", Customer: "black", Metric: "Downtime", Provider: "Adriana", Value: 700, Status: 1},
-		{ID: "asset6", Customer: "white", Metric: "Downtime", Provider: "Michel", Value: 800, Status: 1},
+		{ID: "contract1", Customer: "blue", Metric: "Downtime", Provider: "Tomoko", Value: 300, Status: 1},
+		{ID: "contract2", Customer: "red", Metric: "Downtime", Provider: "Brad", Value: 400, Status: 1},
+		{ID: "contract3", Customer: "green", Metric: "Downtime", Provider: "Jin Soo", Value: 500, Status: 1},
+		{ID: "contract4", Customer: "yellow", Metric: "Downtime", Provider: "Max", Value: 600, Status: 1},
+		{ID: "contract5", Customer: "black", Metric: "Downtime", Provider: "Adriana", Value: 700, Status: 1},
+		{ID: "contract6", Customer: "white", Metric: "Downtime", Provider: "Michel", Value: 800, Status: 1},
 	}
 
 	// Set timeout for writing
