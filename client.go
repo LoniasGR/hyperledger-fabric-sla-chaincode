7fcda4d0c5430f00aa42af03ec8fb7a7de397f5a
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-18 22:37:09 +0200

    Create the first version of a client service

--- client.go
+++ client.go
@@ -7,18 +7,41 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	"github.com/segmentio/kafka-go"
 )
+
+type SLA struct {
+	Customer string `json:"Customer"`
+	ID       string `json:"String"`
+	Metric   string `json:"Metric"`
+	Provider string `json:"Provider"`
+	Status   int    `json:"Status"`
+	Value    int    `json:"Value"`
+}
 
 func main() {
 	os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{"localhost:9092"},
+		Topic:     "SLA",
+		Partition: 0,
+		MinBytes:  10e3, // 10KB
+		MaxBytes:  10e6, // 10MB
+	})
+
 	wallet, err := gateway.NewFileSystemWallet("wallet")
 	if err != nil {
 		fmt.Printf("Failed to create wallet: %s\n", err)
@@ -61,39 +84,36 @@ func main() {
 
 	contract := network.GetContract("SLA")
 
-	result, err := contract.EvaluateTransaction("queryAllCars")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
+	// Cleanup for when the service terminates
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		cleanup(r)
+		os.Exit(0)
+	}()
 
-	result, err = contract.SubmitTransaction("createCar", "CAR10", "VW", "Polo", "Grey", "Mary")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
+	// Poll for new messages
+	for {
 
-	result, err = contract.EvaluateTransaction("queryCar", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
 
-	_, err = contract.SubmitTransaction("changeCarOwner", "CAR10", "Archie")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
+		var s SLA
+		err = json.Unmarshal(m.Value, &s)
+		if err != nil {
+			panic(err.Error())
+		}
 
-	result, err = contract.EvaluateTransaction("queryCar", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
+		result, err := contract.SubmitTransaction("CreateContract", s.ID, s.Customer, s.Metric, s.Provider, fmt.Sprint(s.Value), fmt.Sprint(s.Status))
+		if err != nil {
+			fmt.Printf("Failed to submit transaction: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(result))
 	}
-	fmt.Println(string(result))
 }
 
 func populateWallet(wallet *gateway.Wallet) error {
@@ -131,11 +151,18 @@ func populateWallet(wallet *gateway.Wallet) error {
 		return err
 	}
 
-	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
+	identity := gateway.NewX509Identity("Org1MSP", fmt.Sprint(cert), fmt.Sprint(key))
 
 	err = wallet.Put("appUser", identity)
 	if err != nil {
 		return err
 	}
 	return nil
+
+}
+
+func cleanup(r *kafka.Reader) {
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
 }
