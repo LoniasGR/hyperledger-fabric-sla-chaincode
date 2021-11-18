e12a01c13631b5d03ea51e9268f6783f6a8b5ce5
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-18 22:36:55 +0200

    Rename assets to contracts

--- chaincode-go/chaincode/smartcontract.go
+++ chaincode-go/chaincode/smartcontract.go
@@ -7,12 +7,12 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for managing an Asset
+// SmartContract provides functions for managing an Contract
 type SmartContract struct {
 	contractapi.Contract
 }
 
-// Asset describes basic details of what makes up a simple asset
+// Contract describes basic details of what makes up a simple Contract
 //Insert struct field in alphabetic order => to achieve determinism accross languages
 // golang keeps the order when marshal to json but doesn't order automatically
 type SLA struct {
@@ -24,24 +24,24 @@ type SLA struct {
 	Value    int    `json:"Value"`
 }
 
-// InitLedger adds a base set of assets to the ledger
+// InitLedger adds a base set of Contracts to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	// assets := []SLA{
-	// 	{ID: "asset1", Customer: "blue", Metric: "Downtime", Provider: "Tomoko", Value: 300, Status: 1},
-	// 	{ID: "asset2", Customer: "red", Metric: "Downtime", Provider: "Brad", Value: 400, Status: 1},
-	// 	{ID: "asset3", Customer: "green", Metric: "Downtime", Provider: "Jin Soo", Value: 500, Status: 1},
-	// 	{ID: "asset4", Customer: "yellow", Metric: "Downtime", Provider: "Max", Value: 600, Status: 1},
-	// 	{ID: "asset5", Customer: "black", Metric: "Downtime", Provider: "Adriana", Value: 700, Status: 1},
-	// 	{ID: "asset6", Customer: "white", Metric: "Downtime", Provider: "Michel", Value: 800, Status: 1},
+	// Contracts := []SLA{
+	// 	{ID: "Contract1", Customer: "blue", Metric: "Downtime", Provider: "Tomoko", Value: 300, Status: 1},
+	// 	{ID: "Contract2", Customer: "red", Metric: "Downtime", Provider: "Brad", Value: 400, Status: 1},
+	// 	{ID: "Contract3", Customer: "green", Metric: "Downtime", Provider: "Jin Soo", Value: 500, Status: 1},
+	// 	{ID: "Contract4", Customer: "yellow", Metric: "Downtime", Provider: "Max", Value: 600, Status: 1},
+	// 	{ID: "Contract5", Customer: "black", Metric: "Downtime", Provider: "Adriana", Value: 700, Status: 1},
+	// 	{ID: "Contract6", Customer: "white", Metric: "Downtime", Provider: "Michel", Value: 800, Status: 1},
 	// }
 
-	// for _, asset := range assets {
-	// 	assetJSON, err := json.Marshal(asset)
+	// for _, Contract := range Contracts {
+	// 	ContractJSON, err := json.Marshal(Contract)
 	// 	if err != nil {
 	// 		return err
 	// 	}
 
-	// 	err = ctx.GetStub().PutState(asset.ID, assetJSON)
+	// 	err = ctx.GetStub().PutState(Contract.ID, ContractJSON)
 	// 	if err != nil {
 	// 		return fmt.Errorf("failed to put to world state. %v", err)
 	// 	}
@@ -50,17 +50,17 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	return nil
 }
 
-// CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, customer string, metric string, provider string, value int, status int) error {
-	exists, err := s.AssetExists(ctx, id)
+// CreateContract issues a new Contract to the world state with given details.
+func (s *SmartContract) CreateContract(ctx contractapi.TransactionContextInterface, id string, customer string, metric string, provider string, value int, status int) error {
+	exists, err := s.ContractExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("the asset %s already exists", id)
+		return fmt.Errorf("the Contract %s already exists", id)
 	}
 
-	asset := SLA{
+	Contract := SLA{
 		ID:       id,
 		Customer: customer,
 		Metric:   metric,
@@ -68,45 +68,45 @@ func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface,
 		Value:    value,
 		Status:   status,
 	}
-	assetJSON, err := json.Marshal(asset)
+	ContractJSON, err := json.Marshal(Contract)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	return ctx.GetStub().PutState(id, ContractJSON)
 }
 
-// ReadAsset returns the asset stored in the world state with given id.
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*SLA, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
+// ReadContract returns the Contract stored in the world state with given id.
+func (s *SmartContract) ReadContract(ctx contractapi.TransactionContextInterface, id string) (*SLA, error) {
+	ContractJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read from world state: %v", err)
 	}
-	if assetJSON == nil {
-		return nil, fmt.Errorf("the asset %s does not exist", id)
+	if ContractJSON == nil {
+		return nil, fmt.Errorf("the Contract %s does not exist", id)
 	}
 
-	var asset SLA
-	err = json.Unmarshal(assetJSON, &asset)
+	var Contract SLA
+	err = json.Unmarshal(ContractJSON, &Contract)
 	if err != nil {
 		return nil, err
 	}
 
-	return &asset, nil
+	return &Contract, nil
 }
 
-// UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, customer string, metric string, provider string, value int) error {
-	exists, err := s.AssetExists(ctx, id)
+// UpdateContract updates an existing Contract in the world state with provided parameters.
+func (s *SmartContract) UpdateContract(ctx contractapi.TransactionContextInterface, id string, customer string, metric string, provider string, value int) error {
+	exists, err := s.ContractExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
+		return fmt.Errorf("the Contract %s does not exist", id)
 	}
 
-	// overwriting original asset with new asset
-	asset := SLA{
+	// overwriting original Contract with new Contract
+	Contract := SLA{
 		ID:       id,
 		Customer: customer,
 		Metric:   metric,
@@ -114,77 +114,77 @@ func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface,
 		Value:    value,
 		Status:   status,
 	}
-	assetJSON, err := json.Marshal(asset)
+	ContractJSON, err := json.Marshal(Contract)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	return ctx.GetStub().PutState(id, ContractJSON)
 }
 
-// DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
+// DeleteContract deletes an given Contract from the world state.
+func (s *SmartContract) DeleteContract(ctx contractapi.TransactionContextInterface, id string) error {
+	exists, err := s.ContractExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
+		return fmt.Errorf("the Contract %s does not exist", id)
 	}
 
 	return ctx.GetStub().DelState(id)
 }
 
-// AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
+// ContractExists returns true when Contract with given ID exists in world state
+func (s *SmartContract) ContractExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+	ContractJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
 	}
 
-	return assetJSON != nil, nil
+	return ContractJSON != nil, nil
 }
 
-// SLAViolated transfers the assets from the provider to the customer.
+// SLAViolated transfers the Contracts from the provider to the customer.
 func (s *SmartContract) SLAViolated(ctx contractapi.TransactionContextInterface, id string, newStatus int) error {
-	asset, err := s.ReadAsset(ctx, id)
+	Contract, err := s.ReadContract(ctx, id)
 	if err != nil {
 		return err
 	}
 
-	asset.Status = newStatus
-	assetJSON, err := json.Marshal(asset)
+	Contract.Status = newStatus
+	ContractJSON, err := json.Marshal(Contract)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	return ctx.GetStub().PutState(id, ContractJSON)
 }
 
-// GetAllAssets returns all assets found in world state
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*SLA, error) {
+// GetAllContracts returns all Contracts found in world state
+func (s *SmartContract) GetAllContracts(ctx contractapi.TransactionContextInterface) ([]*SLA, error) {
 	// range query with empty string for startKey and endKey does an
-	// open-ended query of all assets in the chaincode namespace.
+	// open-ended query of all Contracts in the chaincode namespace.
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
 	if err != nil {
 		return nil, err
 	}
 	defer resultsIterator.Close()
 
-	var assets []*SLA
+	var Contracts []*SLA
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
 			return nil, err
 		}
 
-		var asset SLA
-		err = json.Unmarshal(queryResponse.Value, &asset)
+		var Contract SLA
+		err = json.Unmarshal(queryResponse.Value, &Contract)
 		if err != nil {
 			return nil, err
 		}
-		assets = append(assets, &asset)
+		Contracts = append(Contracts, &Contract)
 	}
 
-	return assets, nil
+	return Contracts, nil
 }
