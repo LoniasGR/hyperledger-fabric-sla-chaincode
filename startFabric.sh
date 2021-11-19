56ace849b274af866f6ec9933649857ac5f0e0c1
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-19 15:10:42 +0200

    Fix scripts to be more clean

--- startFabric.sh
+++ startFabric.sh
@@ -10,25 +10,14 @@ set -e
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
-CC_SRC_LANGUAGE=${1:-"go"}
-CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
-
-if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ] ; then
-    CC_SRC_PATH="./chaincode/"
-else
-    echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
-    echo Supported chaincode languages are: go, java, javascript, and typescript
-    exit 1
-fi
-
-# clean out any old identites in the wallets
-rm -rf go/wallet/*
+CC_SRC_LANGUAGE="go"
+CC_SRC_PATH="${PWD}/chaincode"
 
 # launch network; create channel and join peer to channel
-pushd ../test-network #TODO
+pushd ../test-network
 ./network.sh down
-./network.sh up createChannel -ca -s couchdb
-./network.sh deployCC -ccn SLA -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+./network.sh up createChannel -c mychannel -ca
+./network.sh deployCC -ccn SLA -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
 popd
 
 cat <<EOF
