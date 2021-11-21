9830b340aa672b1d0cebe980273e9c3fc1fd8ed4
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-21 17:23:02 +0200

    Update description of `startFabric.sh`

--- startFabric.sh
+++ startFabric.sh
@@ -24,23 +24,11 @@ cat <<EOF
 
 Total setup execution time : $(($(date +%s) - starttime)) secs ...
 
-Next, use the FabCar applications to interact with the deployed FabCar contract.
-The FabCar applications are available in multiple programming languages.
-Follow the instructions for the programming language of your choice:
+Next, use the application to interact with the deployed contract.
 
-Go:
+Start by changing into the "application" directory:
+  cd application
 
-  Start by changing into the "go" directory:
-    cd go
-
-  Then, install dependencies and run the test using:
-    go run client.go
-
-  The test will invoke the sample client app which perform the following:
-    - Import user credentials into the wallet (if they don't already exist there)
-    - Submit a transaction to create a new car
-    - Evaluate a transaction (query) to return details of this car
-    - Submit a transaction to change the owner of this car
-    - Evaluate a transaction (query) to return the updated details of this car
-
+Then, install dependencies and run the test using:
+  go run client.go
 EOF
