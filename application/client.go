4bdbc362329cf81227c5968b57751102d6eaaf07
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-11-24 15:57:05 +0200

    QoL changes to the cllient

--- application/client.go
+++ application/client.go
@@ -13,6 +13,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -25,12 +26,13 @@ import (
 )
 
 type SLA struct {
-	Customer string `json:"Customer"`
-	ID       string `json:"ID"`
-	Metric   string `json:"Metric"`
-	Provider string `json:"Provider"`
-	Status   int    `json:"Status"`
-	Value    int    `json:"Value"`
+	Customer   string `json:"Customer"`
+	ID         string `json:"ID"`
+	Metric     string `json:"Metric"`
+	Provider   string `json:"Provider"`
+	Status     int    `json:"Status"`
+	Value      int    `json:"Value"`
+	Violations int    `json:"Violations"`
 }
 
 type Violation struct {
@@ -46,9 +48,15 @@ func main() {
 		log.Fatalf("Error setting DISCOVERY_AS_LOCALHOST environment variable: %v", err)
 	}
 
+	// Create the topics that will be used (if they don't exist)
+	topics := make([]string, 2)
+	topics[0] = "sla"
+	topics[1] = "sla_violation"
+	createTopic(topics)
+
 	r_SLA := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   []string{"localhost:9092"},
-		Topic:     "sla",
+		Topic:     topics[0],
 		Partition: 0,
 		MinBytes:  10e3, // 10KB
 		MaxBytes:  10e6, // 10MB
@@ -56,21 +64,11 @@ func main() {
 
 	r_Violation := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   []string{"localhost:9092"},
-		Topic:     "sla_violation",
+		Topic:     topics[1],
 		Partition: 0,
 		MinBytes:  10e3, // 10KB
 		MaxBytes:  10e6, // 10MB
 	})
-
-	// Cleanup for when the service terminates
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		cleanup(r_SLA, r_Violation)
-		log.Println("============ application-golang ends ============")
-		os.Exit(0)
-	}()
 
 	wallet, err := gateway.NewFileSystemWallet("wallet")
 	if err != nil {
@@ -110,9 +108,21 @@ func main() {
 
 	contract := network.GetContract("SLA")
 
-	log.Println("--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger")
+	// Cleanup for when the service terminates
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		log.Println("============ application-golang ends ============")
+		showTransactions(contract)
+		cleanup(r_SLA, r_Violation)
+		os.Exit(0)
+	}()
+
+	log.Println("--> Submit Transaction: InitLedger, function the connection with the ledger")
 	result, err := contract.SubmitTransaction("InitLedger")
 	if err != nil {
+		cleanup(r_SLA, r_Violation)
 		log.Fatalf("Failed to Submit transaction: %v", err)
 	}
 	log.Println(string(result))
@@ -120,7 +130,7 @@ func main() {
 	for {
 		deadlineError := errors.New("context deadline exceeded")
 
-		waitTime := time.Now().Add(1 * time.Second)
+		waitTime := time.Now().Add(500 * time.Millisecond)
 		ctx, cancel := context.WithDeadline(context.Background(), waitTime)
 		m, err := r_SLA.ReadMessage(ctx)
 		cancel()
@@ -131,22 +141,25 @@ func main() {
 			var s SLA
 			err = json.Unmarshal(m.Value, &s)
 			if err != nil {
-				panic(err.Error())
+				cleanup(r_SLA, r_Violation)
+				log.Fatalf("Failed to unmarshal: %s\n", err)
 			}
 			log.Println(s)
 			log.Println("--> Submit Transaction: CreateContract, creates new contract with ID, customer, metric, provider, value, and status arguments")
 			result, err := contract.SubmitTransaction("CreateContract", s.ID, s.Customer, s.Metric, s.Provider, fmt.Sprint(s.Value), fmt.Sprint(s.Status))
 			if err != nil {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Failed to submit transaction: %s\n", err)
 			}
 			fmt.Println(string(result))
 		} else if err != nil {
 			if err.Error() != deadlineError.Error() {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Reading message failed: %s\n", err)
 			}
 		}
 
-		waitTime = time.Now().Add(1 * time.Second)
+		waitTime = time.Now().Add(500 * time.Millisecond)
 		ctx, cancel = context.WithDeadline(context.Background(), waitTime)
 		m, err = r_Violation.ReadMessage(ctx)
 		cancel()
@@ -156,30 +169,36 @@ func main() {
 			var v Violation
 			err = json.Unmarshal(m.Value, &v)
 			if err != nil {
-				panic(err.Error())
+				cleanup(r_SLA, r_Violation)
+				log.Fatalf("Unmarshal failed: %s\n", err)
 			}
 			log.Println(v)
 			log.Println("--> Evaluate Transaction: ContractExists, finds existing contract with ID")
 			result, err = contract.EvaluateTransaction("ContractExists", v.ContractID)
 			if err != nil {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Failed to evaluate transaction: %s\n", err)
 			}
 			exists, err := strconv.ParseBool(string(result))
 			if err != nil {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Failed to convert result to boolean: %s\n", err)
 			}
 			if !exists {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Contract with ID: %s does not exist!\n", v.ContractID)
 			}
 
 			log.Println("--> Submit Transaction: SLAViolated, updates contracts details with ID, newStatus")
 			result, err = contract.SubmitTransaction("SLAViolated", v.ContractID)
 			if err != nil {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Failed to submit transaction: %s\n", err)
 			}
 			log.Println(string(result))
 		} else if err != nil {
 			if err.Error() != deadlineError.Error() {
+				cleanup(r_SLA, r_Violation)
 				log.Fatalf("Reading message failed: %s\n", err)
 			}
 		}
@@ -227,11 +246,56 @@ func populateWallet(wallet *gateway.Wallet) error {
 	return wallet.Put("appUser", identity)
 }
 
+func showTransactions(contract *gateway.Contract) {
+	log.Println("--> Evaluate Transaction: GetAllContracts, finds existing contract with ID")
+	result, err := contract.EvaluateTransaction("GetAllContracts")
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %s\n", err)
+	}
+
+	log.Println(string(result))
+}
 func cleanup(r_SLA *kafka.Reader, r_Violation *kafka.Reader) {
-	if err := r_SLA.Close(); err != nil {
-		log.Fatal("failed to close writer:", err)
+	err := r_SLA.Close()
+	if err != nil {
+		log.Fatal("failed to close reader:", err)
 	}
-	if err := r_Violation.Close(); err != nil {
-		log.Fatal("failed to close writer:", err)
+	err = r_Violation.Close()
+	if err != nil {
+		log.Fatal("failed to close reader:", err)
 	}
+}
+
+func createTopic(topics []string) {
+
+	conn, err := kafka.Dial("tcp", "localhost:9092")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{}
+	for _, topic := range topics {
+		topicConfigs = append(topicConfigs, kafka.TopicConfig{
+			Topic:             topic,
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		})
+	}
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		panic(err.Error())
+	}
+
 }
