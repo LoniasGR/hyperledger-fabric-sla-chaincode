21a551cbbb42e181867109a1cb8f638b49dd3757
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-01-13 14:21:47 +0200

    Remove printing from bash client

--- application/runclient.sh
+++ application/runclient.sh
@@ -1,14 +1,10 @@
 
 ENV_DAL=`echo $DISCOVERY_AS_LOCALHOST`
 
-echo "ENV_DAL:"$DISCOVERY_AS_LOCALHOST
-
 if [ "$ENV_DAL" != "true" ]
 then
-	export DISCOVERY_AS_LOCALHOST=true
+    export DISCOVERY_AS_LOCALHOST=true
 fi
-
-echo "DISCOVERY_AS_LOCALHOST="$DISCOVERY_AS_LOCALHOST
 
 echo "run client..."
 
