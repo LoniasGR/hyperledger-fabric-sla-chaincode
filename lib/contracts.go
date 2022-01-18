bb3a8f6194cc1d47e01cf0c52aabf048e97a2b17
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-01-18 13:54:53 +0200

    Update the form of the JSON files

--- lib/contracts.go
+++ lib/contracts.go
@@ -1,10 +1,15 @@
 package lib
 
+import (
+	"time"
+)
+
 type SLA struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	State   string `json:"state"`
-	Details Detail `json:"details"`
+	ID         string     `json:"id"`
+	Name       string     `json:"name"`
+	State      string     `json:"state"`
+	Assessment Assessment `json:"assessment"`
+	Details    Detail     `json:"details"`
 }
 
 type Detail struct {
@@ -13,8 +18,14 @@ type Detail struct {
 	Name       string      `json:"Name"`
 	Provider   Entity      `json:"provider"`
 	Client     Entity      `json:"client"`
-	Creation   string      `json:"creation"`
+	Creation   time.Time   `json:"creation"`
 	Guarantees []Guarantee `json:"guarantees"`
+	Service    string      `json:"service"`
+}
+
+type Assessment struct {
+	FirstExecution time.Time `json:"first_execution"`
+	LastExecution  time.Time `json:"last_execution"`
 }
 
 type Entity struct {
@@ -22,12 +33,30 @@ type Entity struct {
 	Name string `json:"name"`
 }
 
-type Guarantee struct {
+type Importance struct {
 	Name       string `json:"name"`
 	Constraint string `json:"constraint"`
 }
 
+type Guarantee struct {
+	Name       string       `json:"name"`
+	Constraint string       `json:"constraint"`
+	Importance []Importance `json:"importance"`
+}
+
 type Violation struct {
-	ID         string `json:"ID"`
-	ContractID string `json:"ContractID"`
+	ID             string  `json:"id"`
+	SLAID          string  `json:"sla_id"`
+	GuaranteeID    string  `json:"guarantee_id"`
+	Constraint     string  `json:"constraint"`
+	Values         []Value `json:"values"`
+	ImportanceName string  `json:"importanceName"`
+	Importance     int     `json:"importance"`
+	AppID          string  `json:"appID"`
+}
+
+type Value struct {
+	Key      string    `json:"key"`
+	Value    int64     `json:"value"`
+	Datetime time.Time `json:"datetime"`
 }
