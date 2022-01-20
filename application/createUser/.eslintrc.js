8205d0eb0c6bafdd5ebd8e28e73fa5a2154edce4
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-01-20 11:46:56 +0200

    Add user registration as a seperate service

---
+++ application/createUser/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'script'
+  },
+  rules: {
+  }
+}
