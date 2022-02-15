abee6ddd49289144e1e3e576a39dd427434b6f16
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-02-15 18:54:16 +0200

    First iteration of ERC20

---
+++ chaincode/erc20.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+)
+
+// type erc20 interface {
+// 	Name() (string, error)
+// 	Symbol() (string, error)
+// 	Decimals() (uint8, error)
+// 	TotalSupply() (uint64, error)
+// 	BalanceOf(owner string) (uint64, error)
+// 	Transfer(to string, value uint64) (bool, error)
+// 	TransferFrom(from string, value uint64) (bool, error)
+// 	Approve(spender string, value uint64) (bool, error)
+// 	Allowance(owner, spender string) (uint64, error)
+// }
+
+// type erc20_events interface {
+// 	Transfer(_from, _to string, value uint64)
+// 	Approval(owner, spender string, value uint64)
+// }
+
+func (s *SmartContract) Name () (string, error) {
+	//
+	return "", nil
+}
+
+func (s *SmartContract) Symbol () (string, error) {
+	//
+	return "", nil
+}
+
+func (s *SmartContract) Decimals () (uint8, error) {
+	// 
+	return 0, nil
+}
+
+func (s *SmartContract) TotalSupply () (uint64, error) {
+	// 
+	return 0, nil
+}
+
+
