28b8dbb1f5a92c830f59caade1b81c61d25a5443
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-03-03 17:18:16 +0200

    Change kafka to use containers

--- startKafka.sh
+++ startKafka.sh
@@ -1,9 +1,3 @@
-rm -rf /tmp/kafka-logs
-rm -rf /tmp/zookeeper
-
-cd ~/kafka_2.13-3.0.0
-ZOOKEEPER="bin/zookeeper-server-start.sh config/zookeeper.properties"
-KAFKA="bin/kafka-server-start.sh config/server.properties"
-
-
-(trap 'kill 0' SIGINT; eval ${ZOOKEEPER} & eval ${KAFKA})
+pushd ./docker/
+docker-compose up -d 
+popd
