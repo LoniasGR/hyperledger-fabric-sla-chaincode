55b33f9fde4da46aabed7a987769fcb715528824
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-05-10 14:56:47 +0300

    Testing omitempty

--- lib/parts_structs.go
+++ lib/parts_structs.go
@@ -61,7 +61,7 @@ type Part_document_body struct {
 	PalletchangeStops     []Part_timestamp `json:"PalletchangeStops"`
 	PalletchangeTimes     []float32        `json:"PalletchangeTimes"`
 	CarrierID             int              `json:"CarrierID"`
-	TargetCycleTime       *float32         `json:"TargetCycleTime"`
+	TargetCycleTime       float32          `json:"TargetCycleTime,omitempty"`
 	ComponentCode         string           `json:"ComponentCode"`
 	ComponentName         string           `json:"ComponentName"`
 	ComponentIdent        string           `json:"ComponentIdent"`
