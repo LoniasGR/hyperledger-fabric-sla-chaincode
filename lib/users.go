147ccf05767fb99c1179c0b61af4a144fb1980b1
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-10-20 17:07:18 +0300

    Updated users library
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- lib/users.go
+++ lib/users.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -23,8 +24,8 @@ type UserRequest struct {
 	Organization int    `json:"org"`
 }
 
-func UserExistsOrCreate(contract *gateway.Contract, name, id string, org int) (bool, string, error) {
-	result, err := contract.EvaluateTransaction("UserExists", id)
+func UserExistsOrCreate(contract *gateway.Contract, name string, org int) (bool, string, error) {
+	result, err := contract.EvaluateTransaction("UserExists", name)
 	if err != nil {
 		err = fmt.Errorf(string(ColorRed)+"failed to submit transaction: %s\n"+string(ColorReset), err)
 		return false, "", err
@@ -34,63 +35,73 @@ func UserExistsOrCreate(contract *gateway.Contract, name, id string, org int) (b
 		err = fmt.Errorf(string(ColorRed)+"failed to parse boolean: %s\n"+string(ColorReset), err)
 		return false, "", err
 	}
-	if !result_bool {
-		postBody, err := json.Marshal(UserRequest{
-			Username:     name,
-			Organization: org,
-		})
-		if err != nil {
-			err = fmt.Errorf(string(ColorRed)+"failed to marshall post request: %s\n"+string(ColorReset), err)
-			return false, "", err
-		}
-		responseBody := bytes.NewBuffer(postBody)
-		resp, err := http.Post(createUserUrl, "application/json", responseBody)
-		if err != nil {
-			err = fmt.Errorf(string(ColorRed)+"failed to send post request: %s\n"+string(ColorReset), err)
-			return false, "", err
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			err = fmt.Errorf(string(ColorRed)+"failed to get response body: %s\n"+string(ColorReset), err)
-			return false, "", err
-		}
-
-		// We use interface{} since we don't know the contents of the JSON beforehand
-		var responseBodyJSON map[string]interface{}
-		err = json.Unmarshal(body, &responseBodyJSON)
-		if err != nil {
-			err = fmt.Errorf(string(ColorRed)+"failed to unmarshal response body: %s\n"+string(ColorReset), err)
-			return false, "", err
-		}
-		if responseBodyJSON["success"] == true {
-			// get the data of the internal JSON
-			data, ok := responseBodyJSON["data"].(map[string]interface{})
-			if !ok {
-				err = fmt.Errorf(string(ColorRed) + "failed to convert interface to struct\n" + string(ColorReset))
-				return false, "", err
-			}
-			// convert interface{} to string
-			privateKey := fmt.Sprintf("%v", data["privateKey"])
-			publicKey := fmt.Sprintf("%v", data["publicKey"])
-
-			publicKeyStripped := splitCertificate(publicKey)
-			// privateKeyStripped := splitCertificate(privateKey)
-
-			err = saveCertificates(name, privateKey, publicKey)
-			if err != nil {
-				err = fmt.Errorf(string(ColorRed)+"failed to save certificates: %s"+string(ColorReset), err)
-				return false, "", err
-			}
-			return false, strings.ReplaceAll(publicKeyStripped, "\n", ""), nil
-		} else if responseBodyJSON["success"] == false && responseBodyJSON["error"] == "User already exists" {
-			return true, "", nil
-		} else {
-			return false, "", fmt.Errorf(string(ColorRed)+"response failure: %v"+string(ColorReset), responseBodyJSON["error"])
-		}
+	if result_bool {
+		return true, "", nil
 	}
-	return true, "", nil
+
+	postBody, err := json.Marshal(UserRequest{
+		Username:     name,
+		Organization: org,
+	})
+	if err != nil {
+		err = fmt.Errorf(string(ColorRed)+"failed to marshall post request: %s\n"+string(ColorReset), err)
+		return false, "", err
+	}
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post(createUserUrl, "application/json", responseBody)
+	if err != nil {
+		err = fmt.Errorf(string(ColorRed)+"failed to send post request: %s\n"+string(ColorReset), err)
+		return false, "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		err = fmt.Errorf(string(ColorRed)+"failed to get response body: %s\n"+string(ColorReset), err)
+		return false, "", err
+	}
+
+	// We use interface{} since we don't know the contents of the JSON beforehand
+	var responseBodyJSON map[string]interface{}
+	err = json.Unmarshal(body, &responseBodyJSON)
+	if err != nil {
+		err = fmt.Errorf(string(ColorRed)+"failed to unmarshal response body: %s\n"+string(ColorReset), err)
+		return false, "", err
+	}
+	if responseBodyJSON["success"] == false {
+		if responseBodyJSON["error"] == "User already exists" {
+			return false, "", fmt.Errorf("user does not exist on ledger, but exists on user service")
+		}
+		return false, "", fmt.Errorf(string(ColorRed)+"response failure: %v"+string(ColorReset), responseBodyJSON["error"])
+	}
+	// get the data of the internal JSON
+	data, ok := responseBodyJSON["data"].(map[string]interface{})
+	if !ok {
+		err = fmt.Errorf(string(ColorRed) + "failed to convert interface to struct\n" + string(ColorReset))
+		return false, "", err
+	}
+	// convert interface{} to string
+	privateKey := fmt.Sprintf("%v", data["privateKey"])
+	publicKey := fmt.Sprintf("%v", data["publicKey"])
+
+	publicKeyStripped := splitCertificate(publicKey)
+	// privateKeyStripped := splitCertificate(privateKey)
+
+	err = saveCertificates(name, privateKey, publicKey)
+	if err != nil {
+		err = fmt.Errorf(string(ColorRed)+"failed to save certificates: %s"+string(ColorReset), err)
+		return false, "", err
+	}
+
+	publicKeyOneLine := strings.ReplaceAll(publicKeyStripped, "\n", "")
+	log.Println(string(ColorGreen), `--> Submit Transaction:
+					CreateUser, creates new user with name, ID, publickey and an initial balance`, string(ColorReset))
+	_, err = contract.SubmitTransaction("CreateUser", name, publicKeyOneLine, "500")
+	if err != nil {
+		return false, "", fmt.Errorf(string(ColorRed)+"failed to submit transaction: %s\n"+string(ColorReset), err)
+	}
+
+	return false, publicKeyOneLine, nil
 }
 
 // Remove header and footer from key
