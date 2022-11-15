97e7a79b68845cc95281c57dbedd774f39f3563f
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-11-15 14:10:42 +0200

    Update lib
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- lib/connections.go
+++ lib/connections.go
@@ -6,24 +6,23 @@ import (
 	"log"
 	"os"
 
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"github.com/hyperledger/fabric-gateway/pkg/identity"
-
 )
 
 // NewGrpcConnection creates a gRPC connection to the Gateway server.
 func NewGrpcConnection(conf Config) (*grpc.ClientConn, error) {
-	certificate, err := loadCertificate(conf.tlsCertPath)
+	certificate, err := loadCertificate(conf.TlsCertPath)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, err
 	}
 
 	certPool := x509.NewCertPool()
 	certPool.AddCert(certificate)
 	transportCredentials := credentials.NewClientTLSFromCert(certPool, "")
 
-	connection, err := grpc.Dial(conf.peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	connection, err := grpc.Dial(conf.PeerEndpoint, grpc.WithTransportCredentials(transportCredentials))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create gRPC connection: %w", err)
 	}
@@ -32,19 +31,19 @@ func NewGrpcConnection(conf Config) (*grpc.ClientConn, error) {
 }
 
 // NewIdentity creates a client identity for this Gateway connection using an X.509 certificate.
-func NewIdentity(conf Config) *identity.X509Identity {
+func NewIdentity(conf Config) (*identity.X509Identity, error) {
 	log.Print(conf.UserConf.Credentials.Certificate)
 	certificate, err := identity.CertificateFromPEM([]byte(conf.UserConf.Credentials.Certificate))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	id, err := identity.NewX509Identity(conf.UserConf.MspID, certificate)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return id
+	return id, nil
 }
 
 func loadCertificate(filename string) (*x509.Certificate, error) {
@@ -56,18 +55,18 @@ func loadCertificate(filename string) (*x509.Certificate, error) {
 }
 
 // NewSign creates a function that generates a digital signature from a message digest using a private key.
-func NewSign(conf Config) identity.Sign {
+func NewSign(conf Config) (identity.Sign, error) {
 	privateKeyPEM := conf.UserConf.Credentials.PrivateKey
 
 	privateKey, err := identity.PrivateKeyFromPEM([]byte(privateKeyPEM))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	sign, err := identity.NewPrivateKeySign(privateKey)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return sign
+	return sign, nil
 }
--- lib/go.mod
+++ lib/go.mod
@@ -1,3 +1,18 @@
 module github.com/LoniasGR/hyperledger-fabric-sla-chaincode/lib
 
 go 1.18
+
+require (
+	github.com/hyperledger/fabric-gateway v1.1.1
+	google.golang.org/grpc v1.50.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/miekg/pkcs11 v1.1.1 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20221018160656-63c7b68cfc55 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
