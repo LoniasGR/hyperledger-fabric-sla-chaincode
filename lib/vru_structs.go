123ecf7601dad3e95a65d6d973b0cd3c2ba3cd22
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-11-21 13:31:42 +0200

    New vru structs
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- lib/vru_structs.go
+++ lib/vru_structs.go
@@ -1,16 +1,19 @@
 package lib
 
+type Position_s struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
 type Tram_s struct {
-	StationID  int32   `json:"station_id"`
-	Latitude   float64 `json:"latitude"`
-	Longditute float64 `json:"longitude"`
+	StationID int32      `json:"station_id"`
+	Position  Position_s `json:"position"`
 }
 
 type OBU_s struct {
-	StationID  int32   `json:"station_id"`
-	Latitude   float64 `json:"latitude"`
-	Longditute float64 `json:"longitude"`
-	Risk       string  `json:"risk"`
+	StationID int32      `json:"station_id"`
+	Position  Position_s `json:"position"`
+	Risk      string     `json:"risk"`
 }
 
 type VRU struct {
@@ -21,6 +24,7 @@ type VRU struct {
 
 type Risk struct {
 	Critical int `json:"critical"`
+	Warning  int `json:"warning"`
 	HighRisk int `json:"highRisk"`
 	LowRisk  int `json:"lowRisk"`
 	NoRisk   int `json:"noRisk"`
