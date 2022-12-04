46ed41f0bbc54005c46407bbdc3bc8b23ac9e652
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-12-04 05:02:36 +0200

    Improve separation of data between containers
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- scripts/fabric_config.sh
+++ scripts/fabric_config.sh
@@ -59,19 +59,46 @@ function init_storage_volumes() {
 function load_org_config() {
 
   # TODO: Add org here
-  kubectl -n "$ORG0_NS" delete configmap org0-config || true
-  kubectl -n "$ORG1_NS" delete configmap org1-config || true
-  kubectl -n "$ORG2_NS" delete configmap org2-config || true
-  kubectl -n "$ORG3_NS" delete configmap org3-config || true
-  kubectl -n "$ORG4_NS" delete configmap org4-config || true
+  kubectl -n "$ORG0_NS" delete configmap org0-ca-config || true
+  kubectl -n "$ORG1_NS" delete configmap org1-ca-config || true
+  kubectl -n "$ORG2_NS" delete configmap org2-ca-config || true
+  kubectl -n "$ORG3_NS" delete configmap org3-ca-config || true
+  kubectl -n "$ORG4_NS" delete configmap org4-ca-config || true
+
+  # TODO: Add org here
+  kubectl -n "$ORG0_NS" delete configmap org0-orderer-config || true
+  kubectl -n "$ORG1_NS" delete configmap org1-peer-config || true
+  kubectl -n "$ORG2_NS" delete configmap org2-peer-config || true
+  kubectl -n "$ORG3_NS" delete configmap org3-peer-config || true
+  kubectl -n "$ORG4_NS" delete configmap org4-peer-config || true
 
   push_fn "Creating fabric config maps"
   # TODO: Add org here
-  kubectl -n "$ORG0_NS" create configmap org0-config --from-file=config/org0
-  kubectl -n "$ORG1_NS" create configmap org1-config --from-file=config/org1
-  kubectl -n "$ORG2_NS" create configmap org2-config --from-file=config/org2
-  kubectl -n "$ORG3_NS" create configmap org3-config --from-file=config/org3
-  kubectl -n "$ORG4_NS" create configmap org4-config --from-file=config/org4
+  mkdir -p ${TEMP_DIR}/config/org0/ca ${TEMP_DIR}/config/org0/orderer
+  mkdir -p ${TEMP_DIR}/config/org1/ca
+  mkdir -p ${TEMP_DIR}/config/org2/ca
+  mkdir -p ${TEMP_DIR}/config/org3/ca
+  mkdir -p ${TEMP_DIR}/config/org4/ca
+
+  envsubst <config/org0/fabric-ca-server-config.yaml >${TEMP_DIR}/config/org0/ca/fabric-ca-server-config.yaml
+  envsubst <config/org1/fabric-ca-server-config.yaml >${TEMP_DIR}/config/org1/ca/fabric-ca-server-config.yaml
+  envsubst <config/org2/fabric-ca-server-config.yaml >${TEMP_DIR}/config/org2/ca/fabric-ca-server-config.yaml
+  envsubst <config/org3/fabric-ca-server-config.yaml >${TEMP_DIR}/config/org3/ca/fabric-ca-server-config.yaml
+  envsubst <config/org4/fabric-ca-server-config.yaml >${TEMP_DIR}/config/org4/ca/fabric-ca-server-config.yaml
+
+  kubectl -n "$ORG0_NS" create configmap org0-ca-config --from-file ${TEMP_DIR}/config/org0/ca/fabric-ca-server-config.yaml
+  kubectl -n "$ORG1_NS" create configmap org1-ca-config --from-file ${TEMP_DIR}/config/org1/ca/fabric-ca-server-config.yaml
+  kubectl -n "$ORG2_NS" create configmap org2-ca-config --from-file ${TEMP_DIR}/config/org2/ca/fabric-ca-server-config.yaml
+  kubectl -n "$ORG3_NS" create configmap org3-ca-config --from-file ${TEMP_DIR}/config/org3/ca/fabric-ca-server-config.yaml
+  kubectl -n "$ORG4_NS" create configmap org4-ca-config --from-file ${TEMP_DIR}/config/org4/ca/fabric-ca-server-config.yaml
+
+  cp config/org0/configtx-template.yaml config/org0/orderer.yaml ${TEMP_DIR}/config/org0/orderer
+
+  kubectl -n "$ORG0_NS" create configmap org0-orderer-config --from-file=${TEMP_DIR}/config/org0/orderer
+  kubectl -n "$ORG1_NS" create configmap org1-peer-config --from-file=config/org1/core.yaml
+  kubectl -n "$ORG2_NS" create configmap org2-peer-config --from-file=config/org2/core.yaml
+  kubectl -n "$ORG3_NS" create configmap org3-peer-config --from-file=config/org3/core.yaml
+  kubectl -n "$ORG4_NS" create configmap org4-peer-config --from-file=config/org4/core.yaml
 
   pop_fn
 }
